850f479a7ecf8c3d8d40aabaf57d38dc5098e34d
q <comc49092@gmail.com>
2019-05-07 11:45:13 +0300

    Assert adding.
    The palindrom checking and the comment was added.

--- Primes.java
+++ Primes.java
@@ -1,5 +1,5 @@
 // Copyright (C) 2018 Serhii Kostyrko
-final class Primes { // no QA, exceptions checking, serialization capability, annotations, security, immutability, BigInteger, boxing check, static context
+final class Primes { // assert usage, exceptions checking, serialization capability, annotations, security, immutability, BigInteger, boxing check, static context
 
     private final int minNum = 10001; //start (and end) value, length are hard-coded
     private final int maxNum = 99997;
@@ -8,7 +8,6 @@ final class Primes { // no QA, exceptions checking, serialization capability, an
     private int palind;
        
     private int findMaxPrimeNumber(int maxNumPre) {
-
 
         int z;
         int maxNumNew;
@@ -30,22 +29,23 @@ final class Primes { // no QA, exceptions checking, serialization capability, an
             }
 
         }
-        return 10000;
+        return 10001;
     }
 
     private boolean findPalindrome(int firstPrime, int secondPrime) {
 
-        int resultOfMath = firstPrime * secondPrime;
+        int resultOfMul = firstPrime * secondPrime;
 
         String ltrResult = Integer.toString(resultOfMath);
         String rtlResult = new StringBuffer(ltrResult).reverse().toString();
 
         if (ltrResult.equals(rtlResult)) {
 
-            palind = resultOfMath;
+            palind = resultOfMul;
             return false;
 
         } else {
+assert palind == 999949999;
             return true;
         }
     }
