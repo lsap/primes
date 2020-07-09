1c1995312b643a821eff5e03df4de2b71a18edde
lsap <q@DESKTOP-JDPC4HC.localdomain>
2020-07-09 09:48:58 +0300

    Polish.

--- Primes.java
+++ Primes.java
@@ -1,4 +1,5 @@
 // Copyright (C) 2018-2020 Serhii Kostyrko
+import java.util.Arrays;
 import java.util.List;
 
 final class Primes { // assert usage, no exceptions checking, serialization capability
@@ -28,6 +29,44 @@ assert palindrome == 999949999;
             + "\nmultiplier1 = " + multiplier1
             + "\nmultiplier2 = " + multiplier2);
 
+}
+
+static List<Integer> seiveAtkin(int max, int min) {
+    List<Integer> primeNumbers = new java.util.ArrayList<>();
+    int limit = max;
+    boolean[] sieve = new boolean[limit + 1];
+    int limitSqrt = (int)Math.sqrt((double)limit);
+    Arrays.fill(sieve, false);
+    sieve[0] = false;
+    sieve[1] = false;
+    sieve[2] = true;
+    sieve[3] = true;
+    for (int x = 1; x <= limitSqrt; x++) {
+        for (int y = 1; y <= limitSqrt; y++) {
+            int n = (4 * x * x) + (y * y);
+            if (n <= limit && (n % 12 == 1 || n % 12 == 5)) {
+                sieve[n] = !sieve[n];
+            }
+            n = (3 * x * x) - (y * y);
+            if (x > y && n <= limit && (n % 12 == 11)) {
+                sieve[n] = !sieve[n];
+            }
+        }
+    }
+    for (int n = 5; n <= limitSqrt; n++) {
+        if (sieve[n]) {
+            int x = n * n;
+            for (int i = x; i <= limit; i += x) {
+                sieve[i] = false;
+            }
+        }
+    }
+    for(int i  = 0 ; i < sieve.length ; i++) {
+	if(sieve[i]) {
+            primeNumbers.add(i);
+	}
+    }
+    return primeNumbers;
 }
 
 static List<Integer> eratosthenesPrimeNumbers(int max, int min) {
@@ -63,9 +102,11 @@ static boolean palindromeCheck(long i) {
     return true;
 }
 
-public static void main(String[] args) {
+public static void main(String... s) {
 
     var primeNumbers = eratosthenesPrimeNumbers(MAX_MULTIPLIER, MIN_MULTIPLIER);
+    palindrome(primeNumbers);
+    primeNumbers = seiveAtkin(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
     palindrome(primeNumbers);
 }
 }
